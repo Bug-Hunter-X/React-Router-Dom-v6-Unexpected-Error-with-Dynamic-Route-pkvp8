d4f6c8cbe807ae288af89324d7365786f9bcc12c
BugHunter-X <askzenai@gmail.com>
2025-01-01 18:02:06 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,23 @@
+```javascript
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        {/*Added route to handle the missing path*/}
+        <Route path="/*" element={<Navigate to = "/" replace/>}/>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+function Home() { return <h1>Home</h1>; }
+function About() { return <h1>About</h1>; }
+function Contact() { return <h1>Contact</h1>; }
+
+export default App; 
+```
